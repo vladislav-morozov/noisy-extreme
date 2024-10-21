27dcd2347e2a8497ab063b573053f2f4cebc9124
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2024-10-21 20:02:14 +0200

    Cleaning the structure

--- log_WA.m
+++
@@ -1,7 +0,0 @@
-function out = log_WA(log_pdf,log_weight)
-  [n,dim_ksi] = size(log_weight) ;
-
-  mlog_pdf = max(log_pdf+log_weight,[],2) ;
-  out = log(sum(exp(log_pdf+log_weight - repmat(mlog_pdf,1,dim_ksi)),2)) + mlog_pdf ;
-
-end
--- noisyExtreme_gaussianKernelDerivative.m
+++
@@ -1,6 +0,0 @@
-function a = noisyExtreme_gaussianKernelDerivative(x)
-% Computes the derivative of the standard Gaussian kernel
-
-
-    a= 1/sqrt(2*pi).*(-x).*exp(-1/2*x.^2);
-end
