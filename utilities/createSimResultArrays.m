62283d7849373f44a3227dbac411f0af1192c7b3
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2024-10-21 17:10:00 +0200

    Initial implementation of results arrays

---
+++ utilities/createSimResultArrays.m
@@ -0,0 +1,29 @@
+function simResultArrayInstances = ...
+    createSimResultArrays(ciMethodsArray, trueQuantileValues, numSamples)
+% CREATESIMRESULTARRAYS Creates and initializes simResultArray instances.
+%
+% Args:
+%     ciMethodsArray (cell): cell array with methods array, each one will
+%                            be used as a base for a simResultArray 
+%     trueQuantileValues (double, vector): vector of target true quantiles
+%     numSamples (int): number of datasets drawn in the simulation.
+%
+% Returns:
+%     simResultArrayInstances (cell): cell array of initialized 
+%                                     simResultArray instances.
+
+    % Extract the number of methods
+    numInstances = length(ciMethodsArray);
+
+    % Initialize cell array to hold simResultArray instances
+    simResultArrayInstances = cell(1, numInstances);
+    
+    % Loop over methods 
+
+    for methodID = 1:numInstances
+        % Create corresponding simResultArray instance
+        simResultArrayInstances{methodID} = ...
+            simResultArray(ciMethodsArray{methodID}, ...
+            trueQuantileValues, numSamples);
+    end
+end
---
+++ utilities/updateSimResultArrays.m
@@ -0,0 +1,35 @@
+function resultsArray = updateSimResultArrays(resultsArray, ...
+    ciCoversTemp, ciLengthsTemp, estErrorsTemp)
+% UPDATESIMRESULTARRAYS Updates the results array.
+% Inserts the corresponding simulation results into each component 
+% of the resultsArray.
+%
+% Args:
+%     resultsArray (cell): Cell array of simResultArray instances.
+%     ciCoversTemp (double, 3D array): 3D array where rows index datasets,
+%         columns index quantiles, and the third dimension indexes methods. 
+%         Third dimension must match the number of arrays in resultsArray.
+%     ciLengthsTemp (double, 3D array): 3D array of CI lengths.
+%     estErrorsTemp (double, 3D array): 3D array of estimator errors.
+%
+% Returns:
+%     resultsArray (cell): Updated cell array of simResultArray instances.
+
+    % Extract number of methods
+    numMethods = length(resultsArray);
+
+    % Loop over methods (the third dimension of arrays)
+    for methodID = 1:numMethods
+        % Update coverage matrix for each method
+        resultsArray{methodID}.ciCovers = ...
+            squeeze(ciCoversTemp(:, :, methodID));
+        
+        % Update length matrix for each method
+        resultsArray{methodID}.ciLength = ...
+            squeeze(ciLengthsTemp(:, :, methodID));
+        
+        % Update estimator error matrix for each method
+        resultsArray{methodID}.estError = ...
+            squeeze(estErrorsTemp(:, :, methodID));
+    end
+end
