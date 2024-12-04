1fd35862b05afbfab45490bc673a3e2ea84f8a89
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2024-12-04 17:59:07 +0100

    Add more sizes to the intermediate approximation evaluation

--- config/setDGP_IntermediateApproximationQuality.m
+++ config/setDGP_IntermediateApproximationQuality.m
@@ -25,7 +25,7 @@ clear thetaSampler* uSampler*
 
 %% Sample sizes
 
-Ns = [200, 2000, 8000, 16000];
+Ns = [200, 2000, 8000, 16000, 32000];
 % Any value larger than 4 will yield the same results
 Ts = 10;
 
