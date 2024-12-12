bee1330b515412269389ddaca6aa28481aa76f8b
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2024-12-12 17:06:57 +0100

    Update setPlottingParameters.m

--- config/setPlottingParameters.m
+++ config/setPlottingParameters.m
@@ -1,29 +1,35 @@
-%% Plotting parameters
-
-% Set thickness of the lines to use
-plotLineThickness= 1.1;
-
-% Adjust the exponential spacing between quantile
-spacingExponent= 1.2;  
-
-% Dimensions of the plots to use
-plotW = 800;
-plotWsel = 1000;
-plotHall = 400;
-plotH = 270;
-plotHMae = 700;
-
-% Set spacing based on quantiles associated to the distribution 
-spacingStep = 10;
-
-markerStep = 2;
-
-plotHallCoverages = 500;
-plotRatioAllCoverages = 1.8;
-plotWallCoverages= plotRatioAllCoverages*plotHallCoverages;
+% ===========================================================
+% File: setPlottingParameters.m
+% Description: This script sets parameters related to figure creation
+%
+% ===========================================================
+%
+% Project Name: Inference on Extreme Quantiles of Unobserved 
+%               Individual Heterogeneity
+% Developed by: Vladislav Morozov
+%
+% ===========================================================
 
 % Set text interpreters to latex
 set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
 set(groot, 'defaultLegendInterpreter','latex');
 set(groot, 'defaultTextInterpreter', 'latex'); 
-   
+  
+% Line parameters 
+plotLineThickness= 1.1;     % Line thickness 
+colorMode = 'color';        % Colormode: 'color' or 'BW'
+markerStep = 4;             % Step size for adding markers
+
+% X-axis parameters
+spacingExponent= 1.2;       % Exponent for x-axis logarithmic scaling
+spacingStep = 10;           % Step size for x-axis ticks
+
+% Dimension parameters
+plotW = 800;
+plotH = 270;
+plotWsel = 1000;
+plotHall = 400;
+plotHMae = 700;
+plotHallCoverages = 500;
+plotRatioAllCoverages = 1.8;
+plotWallCoverages= plotRatioAllCoverages*plotHallCoverages;
