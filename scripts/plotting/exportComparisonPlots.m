c25e95a4639665c0134df1c753a98c3c889b0ea7
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2025-02-06 14:40:38 +0100

    Add ability to plot various sets of CI per plot set

--- scripts/plotting/exportComparisonPlots.m
+++ scripts/plotting/exportComparisonPlots.m
@@ -42,13 +42,13 @@ elseif plotContext == "tuningParameter"
 end
 
 % --- Plot generation ---
- 
-% Loop through sets 
+
+% Loop through sets
 for plotSetID = 1 : length(plotSet)
     % Extract vectors of (N, T)
     NsPlot = plotSet{plotSetID}.NsPlot;
     TsPlot = plotSet{plotSetID}.TsPlot;
-    
+
     % Extract plotting color mode
     colorField = plotSet{plotSetID}.colorField;
 
@@ -57,181 +57,189 @@ for plotSetID = 1 : length(plotSet)
     numN = length(NsPlot);
     numT = length(TsPlot);
 
-    
-    % Each combination of distributions receives their own plot
+    % Loop through DGPs, plots and interval sets
     for plotID = 1 : plotSet{plotSetID}.maxPlotID
-        for plotDGP_ID = plotSet{plotSetID}.minPlotDGP_ID :  plotSet{plotSetID}.maxPlotDGP_ID
-
-            % Create figure
-            figure('Renderer', 'painters', ...
-                'Position', [50 50 plotSet{plotSetID}.plotW plotSet{plotSetID}.plotH]);
-
-            % Use tight_subplots
-            [has, ~] = tight_subplot(numN, numT,[.07 .036], ...
-                plotSet{plotSetID}.margH,[.05 .04]);
-
-            % Loop over (N, T): each combination receives a subplot
-            for NID = 1:numT
-                for TID = 1:numN
-                    % Create subplot
-                    plotNum = (TID-1)*numT+NID;
-                    axes(has(plotNum))
-
-                    % Extract current sample sizes
-                    N = NsPlot(TID);
-                    T = TsPlot(NID);
-
-                    % Extract current samplers for theta and u
-                    thetaSampler = samplerTable{plotDGP_ID, 1}{1};
-                    uSampler = samplerTable{plotDGP_ID, 2}{1};
-
-                    % Load in corresponding simulation result file
-                    fileName = makeOutputFileName(thetaSampler, uSampler, ...
-                        N, T, ...
-                        numSamples, plotContext);
-                    load(fileName) 
-
-                    % Patches
-                    % Patch colors if these have been changed after creation
-                    switch plotContext
-                        case "methods"
-                            setConfidenceIntervalMethods_CompareIntervals
-                        case "tuningParameter"
-                            setConfidenceIntervalMethods_ExtremeTuningParameters
-                    end
-
-                    % Patch incorrect name on Gbeta
-                    if uSampler.distrLegendName == "G_{\kappa}"
-                        uSampler.distrLegendName = "G_{\beta}";
-                    end
+        for intervalSetID = 1 : length(plotSet{plotSetID}.intervalSets)
 
 
-                    % Set spacing based on the quantiles in the loaded file
-                    spacing = 1:length(targetQuantilesDGP);
-                    spacing= (spacing).^spacingExponent;
-                    spacingMarker = spacing(1:markerStep:end);
+            for plotDGP_ID = plotSet{plotSetID}.minPlotDGP_ID :  plotSet{plotSetID}.maxPlotDGP_ID
 
-                    % Loop through confidence interval methods
-                    for methodID = linePlots{plotID}.firstMethodID:numMethods
-                        % Compute data to plot
+                % Create figure
+                figure('Renderer', 'painters', ...
+                    'Position', [50 50 plotSet{plotSetID}.plotW plotSet{plotSetID}.plotH]);
 
-                        currentData = linePlots{plotID}.computeData(resultsArray, methodID);
+                % Use tight_subplots
+                [has, ~] = tight_subplot(numN, numT,[.07 .036], ...
+                    plotSet{plotSetID}.margH,[.05 .04]);
 
-                        % Change spacing
-                        currentData = currentData .^ linePlots{plotID}.spacingExponentY;
-                        currentDataMarker = currentData(1:markerStep:end);
-                        % Add line to plot
-                        eval(linePlots{plotID}.plotCallLine)
-                        linePlot.HandleVisibility = 'off';
-                        hold on
-                        % Add markers to the plot
-                        eval(linePlots{plotID}.plotCallMarker)
-                        markerPlot.HandleVisibility = 'off';
-                        % Add dummy line with both markers and line for legend
-                        eval(linePlots{plotID}.plotCallLegend)
-                        % Ensure that plotting is done on the same subplot
+                % Loop over (N, T): each combination receives a subplot
+                for NID = 1:numT
+                    for TID = 1:numN
+                        % Create subplot
+                        plotNum = (TID-1)*numT+NID;
+                        axes(has(plotNum))
 
-                    end
+                        % Extract current sample sizes
+                        N = NsPlot(TID);
+                        T = TsPlot(NID);
 
-                    % x-axis: respace ticks according to spacing
-                    xticks([spacing(1:spacingStep:end), spacing(end)])
-                    % Erase x-tick and y-tick labels by default
-                    xticklabels([])
+                        % Extract current samplers for theta and u
+                        thetaSampler = samplerTable{plotDGP_ID, 1}{1};
+                        uSampler = samplerTable{plotDGP_ID, 2}{1};
+
+                        % Load in corresponding simulation result file
+                        fileName = makeOutputFileName(thetaSampler, uSampler, ...
+                            N, T, ...
+                            numSamples, plotContext);
+                        load(fileName)
+
+                        % Patches
+                        % Patch colors if these have been changed after creation
+                        switch plotContext
+                            case "methods"
+                                setConfidenceIntervalMethods_CompareIntervals
+                            case "tuningParameter"
+                                setConfidenceIntervalMethods_ExtremeTuningParameters
+                        end
+
+                        % Patch incorrect name on Gbeta
+                        if uSampler.distrLegendName == "G_{\kappa}"
+                            uSampler.distrLegendName = "G_{\beta}";
+                        end
 
 
-                    defaultYTicks = gca().YTick;
-                    yticklabels([])
+                        % Set spacing based on the quantiles in the loaded file
+                        spacing = 1:length(targetQuantilesDGP);
+                        spacing= (spacing).^spacingExponent;
+                        spacingMarker = spacing(1:markerStep:end);
 
-                    % Set x-axis limits according to spacing
-                    xlim([min(spacing), max(spacing)]);
-                    % Add x-tick labels and axis label only on the bottom plots
-                    if TID == numN
-                        xlabel('Quantile')
-                        xticklabels([targetQuantilesDGP(1:spacingStep:end), targetQuantilesDGP(end)])
-                    end
+                        % Loop through confidence interval methods
+                        for methodID = ...
+                              max(linePlots{plotID}.firstMethodID, ...
+                              plotSet{plotSetID}.intervalSets{intervalSetID}.firstID) ...
+                                :min(numMethods, ...
+                                 plotSet{plotSetID}.intervalSets{intervalSetID}.lastID)
+                            % Compute data to plot
 
-                    % y-axis: change y limits according to the plot
-                    if ~isempty(linePlots{plotID}.yLim)
-                        ylim(linePlots{plotID}.yLim.^ linePlots{plotID}.spacingExponentY);
-                    end
+                            currentData = linePlots{plotID}.computeData(resultsArray, methodID);
 
-                    % y-axis: add y-line if one is requested
-                    if ~isempty(linePlots{plotID}.yLine)
-                        hV = yline(linePlots{plotID}.yLine.^ linePlots{plotID}.spacingExponentY);
-                        hV.HandleVisibility='off';
-                    end
+                            % Change spacing
+                            currentData = currentData .^ linePlots{plotID}.spacingExponentY;
+                            currentDataMarker = currentData(1:markerStep:end);
+                            % Add line to plot
+                            eval(linePlots{plotID}.plotCallLine)
+                            linePlot.HandleVisibility = 'off';
+                            hold on
+                            % Add markers to the plot
+                            eval(linePlots{plotID}.plotCallMarker)
+                            markerPlot.HandleVisibility = 'off';
+                            % Add dummy line with both markers and line for legend
+                            eval(linePlots{plotID}.plotCallLegend)
+                            % Ensure that plotting is done on the same subplot
 
-                    if ~isempty(linePlots{plotID}.yTicks)
-                        yticks(linePlots{plotID}.yTicks.^linePlots{plotID}.spacingExponentY)
-                    end
-                    % y label and ticks only on the left plots
-                    if NID == 1
-                        ylabel(linePlots{plotID}.yLabel)
+                        end
 
-                        %
-                        if isempty(linePlots{plotID}.yTicks)
-                            yticklabels(defaultYTicks)
+                        % x-axis: respace ticks according to spacing
+                        xticks([spacing(1:spacingStep:end), spacing(end)])
+                        % Erase x-tick and y-tick labels by default
+                        xticklabels([])
 
-                        else
 
-                            yticklabels( linePlots{plotID}.yTicks)
+                        defaultYTicks = gca().YTick;
+                        yticklabels([])
+
+                        % Set x-axis limits according to spacing
+                        xlim([min(spacing), max(spacing)]);
+                        % Add x-tick labels and axis label only on the bottom plots
+                        if TID == numN
+                            xlabel('Quantile')
+                            xticklabels([targetQuantilesDGP(1:spacingStep:end), targetQuantilesDGP(end)])
+                        end
+
+                        % y-axis: change y limits according to the plot
+                        if ~isempty(linePlots{plotID}.yLim)
+                            ylim(linePlots{plotID}.yLim.^ linePlots{plotID}.spacingExponentY);
+                        end
+
+                        % y-axis: add y-line if one is requested
+                        if ~isempty(linePlots{plotID}.yLine)
+                            hV = yline(linePlots{plotID}.yLine.^ linePlots{plotID}.spacingExponentY);
+                            hV.HandleVisibility='off';
+                        end
+
+                        if ~isempty(linePlots{plotID}.yTicks)
+                            yticks(linePlots{plotID}.yTicks.^linePlots{plotID}.spacingExponentY)
+                        end
+                        % y label and ticks only on the left plots
+                        if NID == 1
+                            ylabel(linePlots{plotID}.yLabel)
+
+                            %
+                            if isempty(linePlots{plotID}.yTicks)
+                                yticklabels(defaultYTicks)
+
+                            else
+
+                                yticklabels( linePlots{plotID}.yTicks)
+                            end
+
+                        end
+
+                        % Add a title to the subplot and left-justify it
+                        ttl = title("N="+num2str(NsPlot(TID))+", T="+num2str(TsPlot(NID)));
+                        ttl.Units = 'Normalize';
+                        ttl.Position(1) = 0;
+                        ttl.HorizontalAlignment = 'left';
+
+
+                        % Display legend on last plot if legend is required
+                        if (plotSet{plotSetID}.destination == "main" && plotID == 2) ...
+                                || plotSet{plotSetID}.destination ~= "main"
+                            if plotNum == numN*numT && linePlots{plotID}.plotLegend
+                                lgd = legend(...
+                                    'Position', ...
+                                    plotSet{plotSetID}.legendPosition);
+                                fontsize(lgd, ...
+                                    plotSet{plotSetID}.legendFontSize,'points')
+                            end
                         end
 
                     end
-
-                    % Add a title to the subplot and left-justify it
-                    ttl = title("N="+num2str(NsPlot(TID))+", T="+num2str(TsPlot(NID)));
-                    ttl.Units = 'Normalize';
-                    ttl.Position(1) = 0;
-                    ttl.HorizontalAlignment = 'left';
-
-                    
-                    % Display legend on last plot if legend is required
-                    if (plotSet{plotSetID}.destination == "main" && plotID == 2) ...
-                            || plotSet{plotSetID}.destination ~= "main"
-                        if plotNum == numN*numT && linePlots{plotID}.plotLegend
-                            lgd = legend(...
-                                'Position', ...
-                                plotSet{plotSetID}.legendPosition);
-                            fontsize(lgd, ...
-                                plotSet{plotSetID}.legendFontSize,'points')
-                        end
-                    end
-
                 end
+
+                % Add suptitle
+                if plotSet{plotSetID}.destination == "OA"
+                    sgt = sgtitle({linePlots{plotID}.suptitle, ...
+                        "$F = " + thetaSampler.distrLegendName + "$, " + ...
+                        "$G = " + uSampler.distrLegendName + "$", ...
+                        }, ...
+                        'interpreter','latex');
+                else
+                    sgt = sgtitle(linePlots{plotID}.suptitle);
+                end
+
+                % Create figure saving name
+                figureSavingName =   "results/figures/" + ...
+                    plotSet{plotSetID}.colorField + "_" + ...
+                    plotContext + "_" + ...
+                    plotSet{plotSetID}.intervalSets{intervalSetID}.name + "_" + ...
+                    linePlots{plotID}.plotType + "_" + ...
+                    thetaSampler.distrMachineName + "_" + ...
+                    uSampler.distrMachineName;
+
+                % Export as PNG
+                set(gcf, 'PaperPosition', [0 0 1.8*8 8])
+                print(gcf, figureSavingName, '-dpng', '-r300' );
+
+                % Prepare export settings for PDF
+                set(gcf,'Units','Inches');
+                pos = get(gcf,'Position');
+                set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+                % Export as PDF
+                print(gcf, figureSavingName, '-dpdf');
+
+                disp(plotDGP_ID)
             end
-
-            % Add suptitle
-            if plotSet{plotSetID}.destination == "OA"
-                sgt = sgtitle({linePlots{plotID}.suptitle, ...
-                    "$F = " + thetaSampler.distrLegendName + "$, " + ...
-                    "$G = " + uSampler.distrLegendName + "$", ...
-                    }, ...
-                    'interpreter','latex');
-            else
-                sgt = sgtitle(linePlots{plotID}.suptitle);
-            end
-            
-            % Create figure saving name
-            figureSavingName =   "results/figures/" + ...
-                plotSet{plotSetID}.colorField + "_" + ...
-                plotContext + "_" + ...
-                linePlots{plotID}.plotType + "_" + ...
-                thetaSampler.distrMachineName + "_" + ...
-                uSampler.distrMachineName;
-
-            % Export as PNG
-            set(gcf, 'PaperPosition', [0 0 1.8*8 8])
-            print(gcf, figureSavingName, '-dpng', '-r300' );
-
-            % Prepare export settings for PDF
-            set(gcf,'Units','Inches');
-            pos = get(gcf,'Position');
-            set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
-            % Export as PDF
-            print(gcf, figureSavingName, '-dpdf');
-
-            disp(plotDGP_ID)
         end
     end
 end
--- src/config/choosePlotSetsTuningParameters.m
+++ src/config/choosePlotSetsTuningParameters.m
@@ -33,19 +33,38 @@
 %       Font size in legend.
 % - legendPosition (vector):
 %       Position of legend.
+% - intervalSets (arrays):
+%       Blocks of intervals to plot
 % ===========================================================
 
 %% Plots for the Online Appendix
 
-plotSet{1}.destination = "OA";
-plotSet{1}.colorField = 'plottingColor';
-plotSet{1}.NsPlot = Ns;
-plotSet{1}.TsPlot = Ts;
-plotSet{1}.maxPlotID = length(linePlots);
-plotSet{1}.minPlotDGP_ID =  1;
-plotSet{1}.maxPlotDGP_ID =  height(samplerTable);
-plotSet{1}.margH = [.07 .145];
-plotSet{1}.legendFontSize = 6.5;
-plotSet{1}.legendPosition = [0.784, 0.112, 0.1, 0.04];
-plotSet{1}.plotH = 1000;
-plotSet{1}.plotW = 800; 
+plotSet{1}.destination = "OA";                            % destination
+plotSet{1}.colorField = 'plottingColor';                  % color field name
+plotSet{1}.NsPlot = Ns;                                   % vector of N
+plotSet{1}.TsPlot = Ts;                                   % vector of T
+plotSet{1}.maxPlotID = length(linePlots);                 % last line plot
+plotSet{1}.minPlotDGP_ID =  1;                            % first DGP ID
+plotSet{1}.maxPlotDGP_ID =  height(samplerTable);         % last DPG ID
+plotSet{1}.margH = [.07 .175];                            % Vertical margins
+plotSet{1}.legendFontSize = 6.5;                          % Font size
+plotSet{1}.legendPosition = [0.814, 0.892, 0.1, 0.04];    % Position
+plotSet{1}.plotH = 550;                                   % Px height
+plotSet{1}.plotW = 550*1.8;                               % Px width
+
+% Set intervals
+extrSet.firstID = 1;                % Only subsampled
+extrSet.lastID = 8;
+extrSet.name = "subs";
+
+simSet.firstID = 9;                 % Only simulated
+simSet.lastID = 16;
+simSet.name = "sim";
+
+jointSet.firstID = 1;               % Both subsampled and simulated
+jointSet.lastID = 16;
+jointSet.name = "joint";
+
+% Add interval sets
+plotSet{1}.intervalSets = {extrSet, simSet, jointSet};    %  Interval sets
+
